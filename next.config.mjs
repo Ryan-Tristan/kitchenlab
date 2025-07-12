0dc261dddf9f4bec642f943fa7cf8d28aa59b716
Ryan Tristan Wijaya <ryantristan2004@gmail.com>
2025-07-12 14:03:06 +0700

    next.config.mjs fix

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@ import path from 'path';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone',
+    // No custom output setting so Vercel uses its default Next.js build output
 };
 
 export default nextConfig;
