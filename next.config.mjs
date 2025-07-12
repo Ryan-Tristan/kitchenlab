3cdde012f2eb19757cfee181880f4bf2e65b8212
Ryan Tristan Wijaya <ryantristan2004@gmail.com>
2025-07-12 13:31:55 +0700

    added app routing

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 import path from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+    experimental: {
+        appDir: true,
+    },
 };
+
 export default nextConfig;
